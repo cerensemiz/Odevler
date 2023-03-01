f1284e3b88cd0f191e9df2419ce9612c7546dced
cerensemiz <SABAH@DESKTOP-E30TBPJ>
2023-03-01 14:47:59 +0300

    Add project files.

---
+++ RandevuSistemi/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace RandevuSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            btn1 = new Button();
+            button2 = new Button();
+            flt1 = new FlowLayoutPanel();
+            button3 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button4 = new Button();
+            button7 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            flt1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(textBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(textBox2, 0, 1);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 2);
+            tableLayoutPanel1.Controls.Add(flt1, 0, 3);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(452, 518);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Top;
+            textBox1.Location = new Point(3, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(446, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Dock = DockStyle.Top;
+            textBox2.Location = new Point(3, 32);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(446, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(btn1, 0, 0);
+            tableLayoutPanel2.Controls.Add(button2, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Top;
+            tableLayoutPanel2.Location = new Point(3, 61);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(446, 100);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // btn1
+            // 
+            btn1.Dock = DockStyle.Top;
+            btn1.Location = new Point(3, 3);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(217, 43);
+            btn1.TabIndex = 0;
+            btn1.Text = "Mesai Saatlerini Üret";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.Location = new Point(226, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(217, 43);
+            button2.TabIndex = 0;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // flt1
+            // 
+            flt1.Controls.Add(button3);
+            flt1.Controls.Add(button5);
+            flt1.Controls.Add(button6);
+            flt1.Controls.Add(button4);
+            flt1.Controls.Add(button7);
+            flt1.Dock = DockStyle.Fill;
+            flt1.Location = new Point(3, 167);
+            flt1.Name = "flt1";
+            flt1.Padding = new Padding(5);
+            flt1.Size = new Size(446, 348);
+            flt1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(8, 8);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 62);
+            button3.TabIndex = 0;
+            button3.Text = "08:45";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(89, 8);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 62);
+            button5.TabIndex = 0;
+            button5.Text = "button3";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(170, 8);
+            button6.Name = "button6";
+            button6.Size = new Size(84, 62);
+            button6.TabIndex = 0;
+            button6.Text = "button3";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(260, 8);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 62);
+            button4.TabIndex = 0;
+            button4.Text = "button3";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(341, 8);
+            button7.Name = "button7";
+            button7.Size = new Size(83, 60);
+            button7.TabIndex = 1;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(452, 518);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            flt1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button btn1;
+        private Button button2;
+        private FlowLayoutPanel flt1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+    }
+}
