285db00da3547ac9c8fdaeed0fdc58dde8da56aa
cerensemiz <ceren_semiz@outlook.com>
2023-03-13 23:05:28 +0300

    Class oluşturuldu.

---
+++ SinifOlusturma/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace SinifOlusturma
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        //Property= Özellik tanımlamak
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
+//Field = Alan tanımlamak
+//public string FirstName; 
---
+++ SinifOlusturma/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SinifOlusturma 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Viviane";
+            customer.LastName = "Sona";
+
+            Customer customer2 = new Customer
+            { 
+                Id = 2, City= "İstanbul", FirstName= "Derin"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine(); 
+
+        }
+    }
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated!");
+        }
+    }
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Product Updated!");
+        }
+    }
+}
+
+
